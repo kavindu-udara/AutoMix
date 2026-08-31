36230a55b1ae16106122d1366aa499f6d6deb3a0
kavindu-udara <karunasinghesampath38@gmail.com>
2026-09-01 00:34:13 +0530

    Add stems-related columns to Track model and migration script

---
+++ apps/api/prisma/migrations/20260831190407_add_stems/migration.sql
@@ -0,0 +1,7 @@
+-- AlterTable
+ALTER TABLE "Track" ADD COLUMN     "stemsBassKey" TEXT,
+ADD COLUMN     "stemsDrumsKey" TEXT,
+ADD COLUMN     "stemsError" TEXT,
+ADD COLUMN     "stemsOtherKey" TEXT,
+ADD COLUMN     "stemsStatus" TEXT,
+ADD COLUMN     "stemsVocalsKey" TEXT;
